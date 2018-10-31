08f92bc9e618a631066305d8626bea3057caed8f
May Yassin <mayyassin@Mays-MacBook-Pro.local>
2018-10-31 17:16:01 +0300

    task submission

--- src/App.js
+++ src/App.js
@@ -8,13 +8,47 @@ const styles = {
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bold: false,
+      italic: false,
+      underline: false,
+      style: "",
+      color: "black"
+    };
+    this.selectStyle = this.selectStyle.bind(this);
+    this.selectColor = this.selectColor.bind(this);
+  }
+  selectColor(color) {
+    this.setState({ color: color });
+  }
+  selectStyle(style) {
+    switch (style) {
+      case "bold":
+        this.setState({ bold: !this.state.bold });
+        break;
+
+      case "italic":
+        this.setState({ italic: !this.state.italic });
+        break;
+
+      case "underline":
+        this.setState({ underline: !this.state.underline });
+        break;
+    }
+  }
   render() {
     let stylings = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
     let stylingBoxes = stylings.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          style={styles[style]}
+          key={style}
+          onClick={() => this.selectStyle(style)}
+        >
           {style}
         </button>
       );
@@ -25,6 +59,7 @@ class App extends Component {
         <button
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
+          onClick={() => this.selectColor(color)}
         />
       );
     });
@@ -35,7 +70,15 @@ class App extends Component {
         {stylingBoxes}
         <br />
         <br />
-        <textarea />
+
+        <textarea
+          style={{
+            color: this.state.color,
+            fontWeight: this.state.bold ? "bold" : "",
+            fontStyle: this.state.italic ? "italic" : "",
+            textDecorationLine: this.state.underline ? "underline" : ""
+          }}
+        />
         <br />
         {colorBoxes}
       </div>
